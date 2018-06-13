e19d7482f0d3bcc1accc8e6795bb10f26605f997
lxyoutlook <34645156+lxyoutlook@users.noreply.github.com>
2018-06-13 09:13:30 +0800

    Update index.js

--- blue-green-app-deployment/bgd-app/index.js
+++ blue-green-app-deployment/bgd-app/index.js
@@ -31,7 +31,7 @@ app.all('*', function(req, res) {   // serve all other requests
   var icon_name = (app_obj.application_name.indexOf("blue")>= 0)?"Blue-station.png":"Green-station.png";
   res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
   res.write("<html><body style='font-family: Arial'><img align='left' src='./images/Blue-Green-icon.png'>");
-  res.write("<h1><br><br><br>&nbsp;&nbsp;Blue-Green deployments 2018-06-11 v1.0</h1><hr>");
+  res.write("<h1><br><br><br>&nbsp;&nbsp;Blue-Green deployments 2018-06-13 v1.1</h1><hr>");
   res.write("<p><img src='./images/"+icon_name+"'></p>");
   res.write("<hr>");
   res.write("<p><b>Application name:</b> "+ app_obj.application_name+"</p>");
